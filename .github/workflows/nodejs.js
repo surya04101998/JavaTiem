45ea8425c71ffd277e98c2c6c3838377a9e92061
surya04101998 <40798305+surya04101998@users.noreply.github.com>
2019-12-23 14:50:43 +0530

    Update nodejs.js

--- .github/workflows/nodejs.js
+++ .github/workflows/nodejs.js
@@ -9,3 +9,15 @@ json=JSON.stringify(books)
 fs.writeFileSync("1-json.json",json)
 
 jsonBuffer=fs.readFileSync("1-json.json")
+yargs.command({
+  command:'Add',
+  handler:function(){
+    console.log("Bruh Added")
+  }
+})
+yargs.command({
+  command:'Remove',
+  handler:function(){
+    console.log("Removed ra nigga")
+  }
+})
